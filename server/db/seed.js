7d7e860a179faf7d66399bb2fe281898a80d6403
carlschr <cjcarlson136@gmail.com>
2021-12-17 13:41:45 -0600

    Adjusted seed values

--- server/db/seed.js
+++ server/db/seed.js
@@ -32,13 +32,13 @@ async function seed() {
     conversationId: santaigoConvo.id,
     senderId: santiago.id,
     text: "Where are you from?",
-    read: false
+    read: true
   });
   await Message.create({
     conversationId: santaigoConvo.id,
     senderId: thomas.id,
     text: "I'm from New York",
-    read: false
+    read: true
   });
   await Message.create({
     conversationId: santaigoConvo.id,
