ca849b450a500884d9d2ecde7b2df5e639515e23
carlschr <cjcarlson136@gmail.com>
2021-12-17 17:06:13 -0600

    Reordered functions in thunkcreators

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -91,27 +91,27 @@ const sendMessage = (data, body) => {
   });
 };
 
-const updateToRead = async (body) => {
-  const { data } = await axios.put("api/messages", body);
-  return data;
-};
-
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
 export const postMessage = (body) => async (dispatch) => {
   try {
     const data = await saveMessage(body);
-
+    
     if (!body.conversationId) {
       dispatch(addConversation(body.recipientId, data.message));
     } else {
       dispatch(setNewMessage(data.message));
     }
-
+    
     sendMessage(data, body);
   } catch (error) {
     console.error(error);
   }
+};
+
+const updateToRead = async (body) => {
+  const { data } = await axios.put("api/messages", body);
+  return data;
 };
 
 export const readMessages = (conversation) => async (dispatch) => {
