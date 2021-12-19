4a9d34a99f1d0fc612ff1e5bb3a0c34397ffc81b
Christopher Carlson <cjcarlson136@gmail.com>
2021-12-19 13:04:33 -0600

    Remove unshift to improve efficiency
    Co-authored-by: Pranjal soni <43893639+sonipranjal@users.noreply.github.com>

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -13,8 +13,8 @@ export const addMessageToStore = (state, payload) => {
 
   return state.map((convo) => {
     if (convo.id === message.conversationId) {
-      const convoCopy = { ...convo, messages: [...convo.messages] };
-      convoCopy.messages.unshift(message);
+      const convoCopy = { ...convo };
+      convoCopy.messages = [...convo.messages, message];
       convoCopy.latestMessageText = message.text;
       return convoCopy;
     } else {
