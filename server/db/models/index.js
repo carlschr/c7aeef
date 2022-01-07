81e32fa36ea85b7b80d4f2f190e1d8e02d32064a
carlschr <cjcarlson136@gmail.com>
2022-01-07 15:26:54 -0600

    Made new associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,17 +1,22 @@
+const db = require("../db");
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+const UserConversation = db.define("user_conversation", {});
+const Unread = db.define("unread", {});
+
+User.belongsToMany(Conversation, { through: UserConversation });
+Conversation.belongsToMany(User, { through: UserConversation });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
+Message.belongsToMany(User, { through: Unread });
+User.belongsToMany(Message, { through: Unread });
 
 module.exports = {
   User,
   Conversation,
-  Message
+  Message,
 };
