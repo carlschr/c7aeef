2a950277cd408cefed09d71e43aed2c7cade1501
carlschr <cjcarlson136@gmail.com>
2022-01-07 15:26:44 -0600

    Remodeled message and conversation

--- server/db/models/message.js
+++ server/db/models/message.js
@@ -9,10 +9,6 @@ const Message = db.define("message", {
   senderId: {
     type: Sequelize.INTEGER,
     allowNull: false,
-  },
-  read: {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false
   }
 });
 
